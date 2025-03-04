db4b934e1060deb7edfd54c71b3cf46190a4bb91
unknown <beguareyahya01@gmail.com>
2025-03-04 23:54:51 +0000

    feat: smart contract developement

---
+++ hardhat.config.js
@@ -0,0 +1,39 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const INFURA_RPC_URL = process.env.INFURA_RPC_URL;
+const ACCOUNT_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: `${INFURA_RPC_URL}`,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    fuji: {
+      url: `https://api.avax-test.network/ext/bc/C/rpc`,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+      chainId: 43113,
+    },	
+    
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+  sourcify: {
+    enabled: true
+  },
+  // paths: {
+  //   artifacts: "./src/artifacts",
+  // },
+};
